13e49c8c0fd937bc1df931dd9aed988e21ec123b
MarcoRamirezGT <maquito156@gmail.com>
2022-08-22 17:19:40 -0600

    Ejercicio1
    Analisis exploratorio y red neuronal simple

---
+++ Ejercicio1Py/Ejercicio1.py
@@ -0,0 +1,43 @@
+# Import required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
+
+# Import necessary modules
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.metrics import r2_score
+
+
+df = pd.read_csv('train.csv')
+print(type(df['label']))
+
+df.replace(np.nan, 0)
+
+print(df.shape)
+df.describe().transpose()
+
+target_column = ['label']
+predictors = list(set(list(df.columns))-set(target_column))
+df[predictors] = df[predictors]/df[predictors].max()
+df.describe().transpose()
+
+X = df[predictors].values
+y = df[target_column].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.30, random_state=40)
+print(X_train.shape)
+print(X_test.shape)
+
+
+mlp = MLPClassifier(hidden_layer_sizes=(8, 8, 8),
+                    activation='relu', solver='adam', max_iter=500)
+mlp.fit(X_train, y_train)
+
+predict_train = mlp.predict(X_train)
+predict_test = mlp.predict(X_test)
--- ScriptAnalisisExploratorio.R
+++ ScriptAnalisisExploratorio.R
@@ -1,10 +1,34 @@
-# Analisis exploratorio
+library(keras)
+library(mlbench)
+library(dplyr)
+library(magrittr)
+library(neuralnet)
 
-db<-read.csv('train.csv')
+data("BostonHousing")
+data <- BostonHousing
+str(data)
+data %<>% mutate_if(is.factor, as.numeric)
+n <- neuralnet(medv ~ crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+b+lstat,
+               data = data,
+               hidden = c(12,7),
+               linear.output = F,
+               lifesign = 'full',
+               rep=1)
+plot(n,col.hidden = 'darkgreen',     
+     col.hidden.synapse = 'darkgreen',
+     show.weights = F,
+     information = F,
+     fill = 'lightblue')
+data <- as.matrix(data)
+dimnames(data) <- NULL
+set.seed(123)
+ind <- sample(2, nrow(data), replace = T, prob = c(.7, .3))
+training <- data[ind==1,1:13]
+test <- data[ind==2, 1:13]
+trainingtarget <- data[ind==1, 14]
+testtarget <- data[ind==2, 14]
+str(trainingtarget)
+
+str(testtarget)
 
 
-x<-c(1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5)
-table(x)
-summary(x)
-
-summary(as.factor(x))
